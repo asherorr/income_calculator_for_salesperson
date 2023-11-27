edabe9e04020fde40519e1085857b8628b41a999
asherorr <asher@mailfence.com>
2023-11-27 18:37:35 -0500

    This commit will add state_tax and federal_tax parameters to the compensation_generator function, then calculate the compensation before and after taxes.

--- income_calculator.py
+++ income_calculator.py
@@ -4,13 +4,14 @@ import locale
 locale.setlocale(locale.LC_ALL, '')
 
 
-def compensation_generator(annual_base_salary, commission_percentage, list_of_deals_by_revenue):
+def compensation_generator(annual_base_salary, commission_percentage, list_of_deals_by_revenue, state_tax, federal_tax):
     commission_earned_counter = 0
     for deal in list_of_deals_by_revenue:
         earnings = deal * commission_percentage
         commission_earned_counter += earnings
-    ote = (annual_base_salary/12) + commission_earned_counter
-    print(f'You earned: {locale.currency(ote, grouping=True)} this month.')
+    ote_before_tax = (annual_base_salary/12) + commission_earned_counter
+    ote_after_tax = ote_before_tax - (ote_before_tax * (state_tax + federal_tax))
+    print(f'You earned: {locale.currency(ote_after_tax, grouping=True)} this month.')
 
 
-compensation_generator(60000, .03, [38000, 42000, 55000, 20000, 34000])
+compensation_generator(65000, .03, [38000, 42000, 55000, 20000, 34000], .057, .22)
